3fee27e185e637af258cc6d6401f662b8b98f9d4
Todd Willey <todd@lapex.(none)>
2008-09-20 13:52:49 -0400

    Fix the wrong fix.

--- ext_lib/cart_dependency_extension.rb
+++ ext_lib/cart_dependency_extension.rb
@@ -8,7 +8,7 @@ module CartDependencyExtension
           # ActionController::Dispather.new($stdout).prepare_application(true)
           # is aclled on reset! in script/console, but is not prepared
           # when the application is loaded for some reason, so do it manually
-          blk.call if caller.last != /irb/
+          blk.call if caller.last =~ /irb/
         end
       end
     end
